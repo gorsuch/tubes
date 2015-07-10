fe758cec81521038aeb16c008b1bbfe746d6b493
Michael Ehlert <mikey.ehlert@gmail.com>
2015-07-10 16:01:41 -0700

    build a slice of our JSON-ified PIDs

--- pid2json.go
+++ pid2json.go
@@ -5,6 +5,7 @@ package main
 import (
 	"encoding/json"
 	"fmt"
+	//	"reflect"
 
 	"github.com/mitchellh/go-ps"
 	//"strconv"
@@ -18,6 +19,7 @@ type PidValues struct {
 }
 
 var b []byte
+var a []string
 
 // print array of Pidsnapshot
 func tap() {
@@ -25,14 +27,14 @@ func tap() {
 	Pidsnapshot, _ := ps.Processes()
 
 	// Pidsnapshot is now type []ps.Process
-	// Convert to JSON
+	// Convert to slice of JSON formatted elements
 
 	for _, element := range Pidsnapshot {
 		m := PidValues{Pid: element.Pid(), PPid: element.PPid(), Exec: element.Executable()}
 		b, _ = json.Marshal(m)
+		a = append(a, string(b))
 	}
-
-	fmt.Println(string(b))
+	fmt.Println(a)
 }
 
 func Nozzle() {
